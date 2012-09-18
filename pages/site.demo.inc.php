5b8967e7fe76b246bbe36250f7f7ff2f47a5ea27
Joachim Dörr <mail@joachim-doerr.com>
2012-09-18 11:38:35 +0300

    Update pages/site.demo.inc.php

--- pages/site.demo.inc.php
+++ pages/site.demo.inc.php
@@ -64,7 +64,7 @@ $objForm = new mform();
 $objForm->addHeadline(\'Text-Input und Hidden Elemente\');
 
 // text field
-$objForm->addTextField(1,\'REX_VALUE[1]\',array(\'label\'=>\'Name\',\'style\'=>\'width:200px\'));
+$objForm->addTextField(1,\'REX_VALUE[1]\',array(\'label\'=>\'Input\',\'style\'=>\'width:200px\'));
 
 // hidden field
 $objForm->addHiddenField(2,\'hidden feld string\',array(\'label\'=>\'Hidden\',\'style\'=>\'width:200px\'));
@@ -86,29 +86,29 @@ $objForm->addTextReadOnlyField(5,\'string readonly\',array(\'label\'=>\'Readonly
 $objForm->addHeadline(\'Select und Multiselect Elemente\');
 
 // select
-$objForm->addSelectField(6,\'REX_VALUE[6]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Select Name\'));
+$objForm->addSelectField(6,\'REX_VALUE[6]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Select\'));
 
 // select mit ausgelagerten Options, Size und Label
 $objForm->addSelectField(7,\'REX_VALUE[7]\');
 $objForm->addOptions(array(1=>\'test-1\',2=>\'test-2\'));
 $objForm->setSize(5);
-$objForm->setLabel(\'Select Name\');
+$objForm->setLabel(\'Select\');
 
 // multiselect
-$objForm->addMultiSelectField(8,\'REX_VALUE[8]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Multiselect Name\',\'size\'=>\'8\'));
+$objForm->addMultiSelectField(8,\'REX_VALUE[8]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Multiselect\',\'size\'=>\'8\'));
 
 // multiselect
-$objForm->addMultiSelectField(9,\'REX_VALUE[9]\',array(1=>\'test-1\',2=>\'test-2\',3=>\'test-3\',4=>\'test-4\'),array(\'label\'=>\'Multiselect Name\'), \'full\');
+$objForm->addMultiSelectField(9,\'REX_VALUE[9]\',array(1=>\'test-1\',2=>\'test-2\',3=>\'test-3\',4=>\'test-4\'),array(\'label\'=>\'Multiselect\'), \'full\');
 
 
 // headline
 $objForm->addHeadline(\'Radio und Checkbox Elemente\');
 
 // checkbox
-$objForm->addCheckboxField(10,\'REX_VALUE[10]\',array(1=>\'test-1\'),array(\'label\'=>\'Multiselect Name\'));
+$objForm->addCheckboxField(10,\'REX_VALUE[10]\',array(1=>\'test-1\'),array(\'label\'=>\'Checkbox\'));
 
 // radiobox
-$objForm->addRadioField(11,\'REX_VALUE[11]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Multiselect Name\'));
+$objForm->addRadioField(11,\'REX_VALUE[11]\',array(1=>\'test-1\',2=>\'test-2\'),array(\'label\'=>\'Radio Buttons\'));
 
 
 // headline
@@ -154,7 +154,7 @@ $objForm = new mform();
 $objForm->addHeadline(\'Neues Form\');
 
 // text field
-$objForm->addTextField(12,\'REX_VALUE[12]\',array(\'label\'=>\'Name\',\'style\'=>\'width:200px\'));
+$objForm->addTextField(12,\'REX_VALUE[12]\',array(\'label\'=>\'Input\',\'style\'=>\'width:200px\'));
 
 
 // get formular
