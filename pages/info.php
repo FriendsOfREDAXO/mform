49bc86fb30d41d8fd8de88c0fd1487dfea0207a5
Joachim Doerr <mail@joachim-doerr.com>
2016-03-27 23:08:01 +0200

    remove textile requires

--- pages/info.php
+++ pages/info.php
@@ -14,17 +14,15 @@ if (rex_addon::isInstalled('mform') !== true) {
         <p>Dieses Addon erleichtert das Erstellen von Modul Input Formularen.</p>
     ';
 } else {
-    if (rex_addon::get('textile')->isAvailable() === true) {
     $strInfoHeadline = '<h2 class="rex-hl2">'. $this->i18n('help_headline') .'</h2>';
     $strPageContent = '
         <h3>'. $this->i18n('help_subheadline_1') .'</h3>
-        <p>'.  rex_textile::parse($this->i18n('help_infotext_1')) .'</p>
-        <p>'.  rex_textile::parse($this->i18n('help_infotext_2')) .'</p>
+        <p>'.  $this->i18n('help_infotext_1') .'</p>
+        <p>'.  $this->i18n('help_infotext_2') .'</p>
         <h3>'. $this->i18n('help_subheadline_2') .'</h3>
         <p>'.  $this->i18n('help_infotext_3') .'</p>
         <p>'.  $this->i18n('help_infotext_4') .'</p>
     ';
-    }
 }
 
 if (rex_be_controller::getCurrentPagePart(2) == 'info') {
