23c683d8ac09e04e4892a34ed49769b511c538b6
Joachim Doerr <mail@joachim-doerr.com>
2016-07-12 18:26:30 +0200

    add theme css permanent

--- boot.php
+++ boot.php
@@ -10,21 +10,18 @@
 
 if (rex::isBackend())
 {
-  $files = glob(rex_path::addon('mform')."/lib/functions/*.php");
-  array_walk($files,create_function('$file', 'return (is_file ( $file )) ? require_once($file) : false;'));
+    $files = glob(rex_path::addon('mform') . "/lib/functions/*.php");
+    array_walk($files, create_function('$file', 'return (is_file ( $file )) ? require_once($file) : false;'));
 
-  if (rex_request('function', 'string') == 'edit' or rex_request('function', 'string') == 'add')
-  {
     rex_view::addCssFile('?mform_theme=' . rex_addon::get('mform')->getConfig('mform_template'));
     rex_view::addJsFile($this->getAssetsUrl('parsley/i18n/messages.de.js'));
     rex_view::addJsFile($this->getAssetsUrl('parsley/parsley.js'));
 
     rex_extension::register('OUTPUT_FILTER', 'a967_add_parsley');
-  }
 
-  if (rex_request('mform_theme', 'string', '') != '')
-  {
-   mform_generate_css(rex_request('mform_theme', 'string', rex_addon::get('mform')->getConfig('mform_template')));
-   exit;
-  }
+    if (rex_request('mform_theme', 'string', '') != '')
+    {
+        mform_generate_css(rex_request('mform_theme', 'string', rex_addon::get('mform')->getConfig('mform_template')));
+        exit;
+    }
 }
