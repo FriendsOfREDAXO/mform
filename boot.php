991ae5ecfa2bb3e9afab6623515fd6e190554eb7
Joachim Doerr <mail@joachim-doerr.com>
2016-07-21 13:11:21 +0200

    add parsley 2

--- boot.php
+++ boot.php
@@ -8,19 +8,18 @@
  * @license MIT
  */
 
-if (rex::isBackend())
-{
+if (rex::isBackend()) {
     $files = glob(rex_path::addon('mform') . "/lib/functions/*.php");
     array_walk($files, create_function('$file', 'return (is_file ( $file )) ? require_once($file) : false;'));
 
     rex_view::addCssFile('?mform_theme=' . rex_addon::get('mform')->getConfig('mform_template'));
-    rex_view::addJsFile($this->getAssetsUrl('parsley/i18n/messages.de.js'));
-    rex_view::addJsFile($this->getAssetsUrl('parsley/parsley.js'));
+    rex_view::addCssFile($this->getAssetsUrl('parsley/parsley.css'));
+    rex_view::addJsFile($this->getAssetsUrl('parsley/parsley.min.js'));
+    rex_view::addJsFile($this->getAssetsUrl('parsley/extra/validator/dateiso.js'));
+    rex_view::addJsFile($this->getAssetsUrl('parsley/extra/validator/words.js'));
+    rex_view::addJsFile($this->getAssetsUrl('parsley/i18n/de.js'));
 
-    rex_extension::register('OUTPUT_FILTER', 'a967_add_parsley');
-
-    if (rex_request('mform_theme', 'string', '') != '')
-    {
+    if (rex_request('mform_theme', 'string', '') != '') {
         mform_generate_css(rex_request('mform_theme', 'string', rex_addon::get('mform')->getConfig('mform_template')));
         exit;
     }
