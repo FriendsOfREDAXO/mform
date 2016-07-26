279b0f77fc7ae41ea2cb6c9303fd20f55628a43f
Joachim Doerr <mail@joachim-doerr.com>
2016-07-26 22:45:54 +0200

    use helper and i18n in pages

--- pages/demo.demo_expert.php
+++ pages/demo.demo_expert.php
@@ -8,45 +8,14 @@
  * @license MIT
  */
 
-$fileType = 'expert';
-
-$demo_referenz = '
-        <p>Die "Experten Beispiele" zeigen exemplarisch, dass MForm für mehr als nur für die Standards genutzt werden kann.</p>
-    ';
 // parse info fragment
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('info'), false);
-$fragment->setVar('body', $demo_referenz, false);
+$fragment->setVar('body', '<p>'.$this->i18n('example_description_expert').'</p>', false);
 echo $fragment->parse('core/page/section.php');
-
-$return = '';
-foreach (scandir(rex_path::addon('mform', 'pages/examples')) as $file) {
-    if (is_dir($file)) {
-        continue;
-    }
-    if (strpos($file, $fileType) !== false && strpos($file, 'output') === false) {
-
-        // add input
-        $content = '<h3>Modul Eingabe</h3>' . rex_string::highlight(file_get_contents(rex_path::addon('mform', 'pages/examples/' . $file)));
-
-        if (file_exists(rex_path::addon('mform', 'pages/examples/' . pathinfo($file, PATHINFO_FILENAME) . '_output.ini'))) {
-            // add output
-            $content .= '<h3>Modul Ausgabe</h3>' . rex_string::highlight(file_get_contents(rex_path::addon('mform', 'pages/examples/' . pathinfo($file, PATHINFO_FILENAME) . '_output.ini')));
-        }
-
-        // parse info fragment
-        $fragment = new rex_fragment();
-        $fragment->setVar('title', $this->i18n('example_' . preg_replace('/\d+/u', '', pathinfo($file, PATHINFO_FILENAME))));
-        $fragment->setVar('content', '<div class="span" style="padding: 0 20px 10px 20px">' . $content . '</div>', false);
-        $fragment->setVar('collapse', true);
-        $fragment->setVar('collapsed', true);
-        $content = $fragment->parse('core/page/section.php');
-        $return .= $content;
-    }
-}
 
 // parse info fragment
 $fragment = new rex_fragment();
-$fragment->setVar('title', $this->i18n('demo_' . $fileType), false);
-$fragment->setVar('body', $return, false);
+$fragment->setVar('title', $this->i18n('demo_expert'), false);
+$fragment->setVar('body', MFormPageHelper::exchangeExamples('expert'), false);
 echo $fragment->parse('core/page/section.php');
