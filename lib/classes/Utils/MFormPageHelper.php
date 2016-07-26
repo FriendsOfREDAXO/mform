73fa9eaa0735120b53a744bd0796ec5fa92597a3
Joachim Doerr <mail@joachim-doerr.com>
2016-07-26 22:45:23 +0200

    add mform helper

---
+++ lib/classes/Utils/MFormPageHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: joachimdoerr
+ * Date: 26.07.16
+ * Time: 22:26
+ */
+class MFormPageHelper
+{
+    public static function exchangeExamples($type)
+    {
+        $return = '';
+        foreach (scandir(rex_path::addon('mform', 'pages/examples')) as $file) {
+            if (is_dir($file)) {
+                continue;
+            }
+            if (strpos($file, $type) !== false && strpos($file, 'output') === false) {
+
+                // add input
+                $content = '<h3>'.rex_i18n::msg('mform_modul_input').'</h3>' . rex_string::highlight(file_get_contents(rex_path::addon('mform', 'pages/examples/' . $file)));
+
+                if (file_exists(rex_path::addon('mform', 'pages/examples/' . pathinfo($file, PATHINFO_FILENAME) . '_output.ini'))) {
+                    // add output
+                    $content .= '<h3>'.rex_i18n::msg('mform_modul_output').'</h3>' . rex_string::highlight(file_get_contents(rex_path::addon('mform', 'pages/examples/' . pathinfo($file, PATHINFO_FILENAME) . '_output.ini')));
+                }
+
+                // parse info fragment
+                $fragment = new rex_fragment();
+                $fragment->setVar('title', rex_i18n::msg('mform_example_' . preg_replace('/\d+/u', '', pathinfo($file, PATHINFO_FILENAME))));
+                $fragment->setVar('content', '<div class="span" style="padding: 0 20px 10px 20px">' . $content . '</div>', false);
+                $fragment->setVar('collapse', true);
+                $fragment->setVar('collapsed', true);
+                $content = $fragment->parse('core/page/section.php');
+                $return .= $content;
+            }
+        }
+        return $return;
+    }
+}
