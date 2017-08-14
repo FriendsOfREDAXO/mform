08777327db2d154a0bd3bc152d23bb881c8dd222
Joachim Dörr <mail@joachim-doerr.com>
2017-08-14 10:10:48 +0200

    remove wiki link

--- pages/info.php
+++ pages/info.php
@@ -9,5 +9,4 @@ $headline = '<h3>'. rex_i18n::msg('mform_help_subheadline_1') .'</h3>';
 $content = '<p>'.  rex_i18n::msg('mform_help_infotext_1') .'</p>
     <p>'.  rex_i18n::msg('mform_help_infotext_2') .'</p>
     <p>'.  rex_i18n::msg('mform_help_infotext_3') .'</p>
-    <a href="https://github.com/FriendsOfREDAXO/mform/" target="_blank">'. rex_i18n::msg('mform_github') .'</a>
-    <br><a href="https://github.com/FriendsOfREDAXO/mform/wiki" target="_blank">'. rex_i18n::msg('mform_wiki') .'</a>';
+    <a href="https://github.com/FriendsOfREDAXO/mform/" target="_blank">'. rex_i18n::msg('mform_github') .'</a>';
