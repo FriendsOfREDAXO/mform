f7a673d8f682db3bb7de5c04d34e821bd9e54fd0
Joachim Dörr <mail@joachim-doerr.com>
2019-03-14 00:49:03 +0100

    extend from abstract not from medialist

--- lib/yform/value/imagelist.php
+++ lib/yform/value/imagelist.php
@@ -5,7 +5,7 @@
  * Time: 22:10
  */
 
-class rex_yform_value_imagelist extends rex_yform_value_be_media
+class rex_yform_value_imagelist extends rex_yform_value_abstract
 {
     public function enterObject()
     {
@@ -38,4 +38,53 @@ class rex_yform_value_imagelist extends rex_yform_value_be_media
         ];
     }
 
+    public static function getListValue($params)
+    {
+        $files = explode(',', $params['subject']);
+
+        if (count($files) == 1) {
+            $filename = $params['subject'];
+            if (strlen($params['subject']) > 16) {
+                $filename = mb_substr($params['subject'], 0, 6) . ' ... ' . mb_substr($params['subject'], -6);
+            }
+            $return[] = '<span style="white-space:nowrap;" title="' . rex_escape($params['subject']) . '">' . $filename . '</span>';
+        } else {
+            foreach ($files as $file) {
+                $filename = $file;
+                if (strlen($file) > 16) {
+                    $filename = mb_substr($file, 0, 6) . ' ... ' . mb_substr($file, -6) . '</span>';
+                }
+                $return[] = '<span style="white-space:nowrap;" title="' . htmlspecialchars($file) . '">' . $filename . '</span>';
+            }
+        }
+
+        return implode('<br />', $return);
+    }
+
+    public static function getSearchField($params)
+    {
+        $params['searchForm']->setValueField('text', ['name' => $params['field']->getName(), 'label' => $params['field']->getLabel()]);
+    }
+
+    public static function getSearchFilter($params)
+    {
+        $sql = rex_sql::factory();
+        $value = $params['value'];
+        $field = $params['field']->getName();
+
+        if ($value == '(empty)') {
+            return ' (' . $sql->escapeIdentifier($field) . ' = "" or ' . $sql->escapeIdentifier($field) . ' IS NULL) ';
+        }
+        if ($value == '!(empty)') {
+            return ' (' . $sql->escapeIdentifier($field) . ' <> "" and ' . $sql->escapeIdentifier($field) . ' IS NOT NULL) ';
+        }
+
+        $pos = strpos($value, '*');
+        if ($pos !== false) {
+            $value = str_replace('%', '\%', $value);
+            $value = str_replace('*', '%', $value);
+            return $sql->escapeIdentifier($field) . ' LIKE ' . $sql->escape($value);
+        }
+        return $sql->escapeIdentifier($field) . ' = ' . $sql->escape($value);
+    }
 }
