3e8993300100dd5b1494bc958bb1823e9231f7f9
Thomas Skerbis <thomas.skerbis@mac.com>
2021-03-26 23:44:14 +0100

    deleted set config

--- install.php
+++ install.php
@@ -5,10 +5,5 @@
  * @license MIT
  */
 
-// set default template
-if (!$this->hasConfig()) {
-    $this->setConfig('mform_theme', 'default_theme');
-}
-
 // copy data directory
 rex_dir::copy($this->getPath('data'), $this->getDataPath());
