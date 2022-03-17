bd6fa64eb996468cabcb2d81e1d7b0622fb19ff5
Thomas Skerbis <thomas.skerbis@mac.com>
2022-03-17 01:23:38 +0100

    succsess message

--- update.php
+++ update.php
@@ -4,3 +4,5 @@ $pluginDocs = __DIR__.'/plugins/docs';
 if (file_exists($pluginDocs)) {
     rex_dir::delete($pluginDocs);
 }
+$addon = rex_addon::get("mform");
+$addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("mform_change_message") . '</strong>');
