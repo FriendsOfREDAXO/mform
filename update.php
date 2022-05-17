7a1a87b55dc368a30d414fa47e2ce95f30dba783
Thomas Skerbis <thomas.skerbis@mac.com>
2022-05-17 16:45:33 +0200

    Update update.php

--- update.php
+++ update.php
@@ -1,3 +1,9 @@
-<?php 
+<?php
+// old plugin docs still exists ? -> delete
+$pluginDocs = __DIR__.'/plugins/docs';
+if (file_exists($pluginDocs)) {
+    rex_dir::delete($pluginDocs);
+}
+
 $addon = rex_addon::get('mform');
 $addon->setProperty('successmsg', '<br><strong>' . rex_i18n::msg("mform_change_message") . '</strong>');
