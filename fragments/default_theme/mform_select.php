d27a6456b5117ba6d766731a1a4d005832d5f232
Thomas Skerbis <thomas.skerbis@mac.com>
2024-03-21 19:39:13 +0100

    convert to variable

--- fragments/default_theme/mform_select.php
+++ fragments/default_theme/mform_select.php
@@ -1,8 +1,8 @@
 <?php
-
+$attributes  ='';
 switch ($this->type) {
     case 'multiselect':
-        $this->attributes .= ' multiple';
+       $attributes .= ' multiple';
         // no break
     case 'select':
         foreach (rex_view::getJsFiles() as $file) {
@@ -12,10 +12,10 @@ switch ($this->type) {
                 $this->class .= ' selectpicker';
             }
         }
-        echo $this->javascript . '<select id="' . $this->id . '" name="REX_INPUT_VALUE' . $this->varId . '" class="' . $this->class . '" ' . $this->attributes . ' data-selected="' . $this->value . '">' . $this->options . '</select>' . $this->hidden;
+        echo $this->javascript . '<select id="' . $this->id . '" name="REX_INPUT_VALUE' . $this->varId . '" class="' . $this->class . '" ' . $attributes . ' data-selected="' . $this->value . '">' . $this->options . '</select>' . $this->hidden;
         break;
     case 'option':
-        echo '<option value="' . $this->value . '"' . $this->attributes . '>' . $this->label . '</option>';
+        echo '<option value="' . $this->value . '"' . $attributes . '>' . $this->label . '</option>';
         break;
     case 'optgroup':
         echo '<optgroup label="' . $this->label . '">' . $this->options . '</optgroup>';
