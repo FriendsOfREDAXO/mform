04e6f80c6c76ab31f3fd0ccb0d8f6f9bf43c94af
Thomas Skerbis <thomas.skerbis@mac.com>
2024-03-26 21:20:25 +0100

    Update mform_select.php

--- fragments/default_theme/mform_select.php
+++ fragments/default_theme/mform_select.php
@@ -2,14 +2,14 @@
 
 switch ($this->type) {
     case 'multiselect':
-        $this->attributes .= ' multiple';
-        // no break
+        $this->setVar('attriutes',$this->attributes.' multiple');
+
     case 'select':
         foreach (rex_view::getJsFiles() as $file) {
             if (str_contains($file, 'bootstrap-select')
                 && !str_contains($this->class, 'selectpicker')
                 && !str_contains($this->class, 'none-selectpicker')) {
-                $this->class .= ' selectpicker';
+                $this->setVar('class',$this->class.' selectpicker');
             }
         }
         echo $this->javascript . '<select id="' . $this->id . '" name="REX_INPUT_VALUE' . $this->varId . '" class="' . $this->class . '" ' . $this->attributes . ' data-selected="' . $this->value . '">' . $this->options . '</select>' . $this->hidden;
