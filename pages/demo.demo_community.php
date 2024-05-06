b443812edafd12eff9603b31c493b111fc49b09e
Alexander Walther <mail@alexplus.de>
2024-05-06 11:11:31 +0200

    Docs Menü

--- pages/demo.demo_community.php
+++ pages/demo.demo_community.php
@@ -7,6 +7,8 @@
 
 use MForm\Utils\MFormPageHelper;
 
+echo rex_view::title(rex_i18n::msg('mform_title') . ' ' . rex_i18n::msg('mform_' . rex_be_controller::getCurrentPagePart(2)));
+
 // parse info fragment
 $fragment = new rex_fragment();
 $fragment->setVar('title', $this->i18n('info'), false);
