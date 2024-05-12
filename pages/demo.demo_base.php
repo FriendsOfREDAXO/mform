fc3e86863a2a2a1dac1e285cc7bb481f995a008a
Alexander Walther <mail@alexplus.de>
2024-05-12 17:06:50 +0200

    Verschachtelung in Docs vermeiden

--- pages/demo.demo_base.php
+++ pages/demo.demo_base.php
@@ -13,8 +13,4 @@ $fragment->setVar('title', rex_i18n::msg('mform_info'), false);
 $fragment->setVar('body', '<p>' . rex_i18n::msg('mform_example_description_base') . '</p>', false);
 echo $fragment->parse('core/page/section.php');
 
-// parse info fragment
-$fragment = new rex_fragment();
-$fragment->setVar('title', rex_i18n::msg('mform_demo_base'), false);
-$fragment->setVar('body', MFormPageHelper::exchangeExamples('base'), false);
-echo $fragment->parse('core/page/section.php');
+echo MFormPageHelper::exchangeExamples('base');
